ddc846724b0333f7d1409171606f8fb2f60dc6c4
hiranami <masa.namihira@gmail.com>
2020-07-15 14:10:55 +0900

    first commit

---
+++ src/main/kotlin/com/future/trade/williams/WilliamsApplication.kt
@@ -0,0 +1,11 @@
+package com.future.trade.williams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WilliamsApplication
+
+fun main(args: Array<String>) {
+	runApplication<WilliamsApplication>(*args)
+}
---
+++ src/test/kotlin/com/future/trade/williams/WilliamsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.future.trade.williams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WilliamsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
